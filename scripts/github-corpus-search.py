a27e52da7525be7cc3961aac1ff5900ce689d323
Robb Shecter <robb@public.law>
2024-03-18 00:05:52 -0600

    refactoring, mostly

--- scripts/github-corpus-search.py
+++ scripts/github-corpus-search.py
@@ -1,29 +1,35 @@
 import os
+
 import requests
+from rich import print
 
-def count_github_repositories(search_term: str):
-    api_token = os.getenv('GITHUB_API_TOKEN')
-    search_endpoint = 'https://api.github.com/search/repositories'
 
+REPO_SEARCH_ENDPOINT = 'https://api.github.com/search/repositories'
+API_TOKEN            = os.getenv('GITHUB_API_TOKEN')
+if not API_TOKEN:
+    raise ValueError("Please set the GITHUB_API_TOKEN environment variable")
+
+
+def count_github_repositories(search_term: str) -> int:
     # Prepare the headers and query parameters
     headers = {
-        'Authorization': f'token {api_token}',
+        'Authorization': f'token {API_TOKEN}',
         'Accept': 'application/vnd.github.v3+json',
     }
     params = {'q': search_term}
 
     # Perform the search request
-    response = requests.get(search_endpoint, headers=headers, params=params)
+    response = requests.get(REPO_SEARCH_ENDPOINT, headers=headers, params=params)
 
     # Check the response status
     if response.status_code == 200:
         data = response.json()
-        total_count = data['total_count']
+        return data['total_count']
         print(f"Total repositories found: {total_count}")
     else:
-        print(f"Failed to fetch data. HTTP Status Code: {response.status_code}")
+        raise ValueError(f"Failed to perform the search: {response.status_code}")
 
 
 # Example usage with a search term
 search_term = 'topic:hebrew'
-count_github_repositories(search_term)
+print(search_term, count_github_repositories(search_term))
